cbc638108727c7444bb5714cad3f57e96710ae4f
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-06-24 16:24:59 +0000

    this is my initial work on the ui

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const typingTest = document.querySelector('.typing-test'),
+inpField = document.querySelector('.wrapper .input-field');
+
+
+
+function randomParagraph() {
+    console.log(paragraphs[0]);
+    // let ranIndex = Math.floor(Math.random() * paragraphs.length);
+    // paragraphs[ranIndex].split('').forEach(span =>{
+    //     let spanTag =  `<span>${span}</span>`;
+    //     typingTest.innerHTML += spanTag;
+    // });
+};
+
+randomParagraph();
